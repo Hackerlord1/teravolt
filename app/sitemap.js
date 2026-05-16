65c292ca05e22f503e6a99159e0576d2795682fd
Hackerlord1 <hemankipkoechchirchir@gmail.com>
2026-05-16 21:13:44 +0300

    adding a sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,35 +1,17 @@
+import { blogPosts } from "@/lib/blogData";
 
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+export default function sitemap() {
+  const baseUrl = "https://www.teravoltdigital.website";
 
-  
-  <url>
-    <loc>https://www.teravoltdigital.website/</loc>
-  </url>
+  const blogUrls = blogPosts.map(post => ({
+    url: `${baseUrl}/blog/${post.slug}`,
+  }));
 
-  
-  <url>
-    <loc>https://www.teravoltdigital.website/blog</loc>
-  </url>
-
-  <url>
-    <loc>https://www.teravoltdigital.website/portfolio</loc>
-  </url>
-
-  <url>
-    <loc>https://www.teravoltdigital.website/services</loc>
-  </url>
-
-  
-  <url>
-    <loc>https://www.teravoltdigital.website/blog/[slug]</loc>
-  </url>
-
-  <url>
-    <loc>https://www.teravoltdigital.website/portfolio/[slug]</loc>
-  </url>
-
-  <url>
-    <loc>https://www.teravoltdigital.website/services/[slug]</loc>
-  </url>
-
-</urlset>
+  return [
+    { url: baseUrl },
+    { url: `${baseUrl}/blog` },
+    { url: `${baseUrl}/portfolio` },
+    { url: `${baseUrl}/services` },
+    ...blogUrls,
+  ];
+}
