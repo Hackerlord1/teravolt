d7e8b5be41367d6fb05b8bd7af25951efb887c76
Hackerlord1 <hemankipkoechchirchir@gmail.com>
2026-05-16 21:03:28 +0300

    adding a sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,35 @@
+
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+  
+  <url>
+    <loc>https://www.teravoltdigital.website/</loc>
+  </url>
+
+  
+  <url>
+    <loc>https://www.teravoltdigital.website/blog</loc>
+  </url>
+
+  <url>
+    <loc>https://www.teravoltdigital.website/portfolio</loc>
+  </url>
+
+  <url>
+    <loc>https://www.teravoltdigital.website/services</loc>
+  </url>
+
+  
+  <url>
+    <loc>https://www.teravoltdigital.website/blog/[slug]</loc>
+  </url>
+
+  <url>
+    <loc>https://www.teravoltdigital.website/portfolio/[slug]</loc>
+  </url>
+
+  <url>
+    <loc>https://www.teravoltdigital.website/services/[slug]</loc>
+  </url>
+
+</urlset>
