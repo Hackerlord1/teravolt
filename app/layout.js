dd4c0df41fa195dba9c3356ce8829340f2828e63
Hackerlord1 <hemankipkoechchirchir@gmail.com>
2026-05-22 10:52:03 +0300

    updates on footer/header duplication

--- app/layout.js
+++ app/layout.js
@@ -19,24 +19,44 @@ export default function RootLayout({ children }) {
         <Footer />
         <BackToTop />
 
+        {/* Tawk.to Live Chat - Positioned at Bottom Left */}
         <Script
           id="tawk-to"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-              Tawk_API.onLoad = function(){
-                Tawk_API.setAttributes({
-                  'no-logging': true
-                }, function(error){});
+              var Tawk_API = Tawk_API || {}, 
+                  Tawk_LoadStart = new Date();
+
+              // Move Tawk.to widget to Bottom Left
+              Tawk_API.customStyle = {
+                visibility: {
+                  desktop: {
+                    position: 'bl',      // 'bl' = bottom left
+                    xOffset: 25,         // Distance from left
+                    yOffset: 25          // Distance from bottom
+                  },
+                  mobile: {
+                    position: 'bl',      // Bottom left on mobile
+                    xOffset: 15,
+                    yOffset: 15
+                  }
+                }
               };
+
+              // Optional: Hide default widget if needed and show only icon
+              Tawk_API.onLoad = function() {
+                // You can add more customizations here if needed
+              };
+
               (function(){
-                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-                s1.async=true;
-                s1.src='https://embed.tawk.to/${TAWK_SCRIPT_ID}';
-                s1.charset='UTF-8';
-                s1.setAttribute('crossorigin','*');
-                s0.parentNode.insertBefore(s1,s0);
+                var s1 = document.createElement("script"),
+                    s0 = document.getElementsByTagName("script")[0];
+                s1.async = true;
+                s1.src = 'https://embed.tawk.to/${TAWK_SCRIPT_ID}';
+                s1.charset = 'UTF-8';
+                s1.setAttribute('crossorigin', '*');
+                s0.parentNode.insertBefore(s1, s0);
               })();
             `,
           }}
